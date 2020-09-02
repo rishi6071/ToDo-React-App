f97bee71cfef7b634a991ba52eeeb1f255e952d9
rishi6071 <52880158+rishi6071@users.noreply.github.com>
2020-09-03 00:27:14 +0530

    Updates

---
+++ precache-manifest.05a03b24194d37d1ba405b7ee660cbe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8ca33a612c967c880280632d65fefe",
+    "url": "/todolist-react-app/index.html"
+  },
+  {
+    "revision": "f14c590110004b1e1866",
+    "url": "/todolist-react-app/static/css/main.1225e5bb.chunk.css"
+  },
+  {
+    "revision": "062bdb790022a720bb52",
+    "url": "/todolist-react-app/static/js/2.f23b9c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react-app/static/js/2.f23b9c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14c590110004b1e1866",
+    "url": "/todolist-react-app/static/js/main.9161b897.chunk.js"
+  },
+  {
+    "revision": "e9dd23c9c0a0e377545d",
+    "url": "/todolist-react-app/static/js/runtime-main.f3476552.js"
+  }
+]);
